aff063041b888380e68284c1a212568acea0d0b4
crypto0627 <jake0627a1@gmail.com>
2024-02-15 15:04:27 +0800

    feat: golang sql init

---
+++ backend/db/sqlc/account_test.go
@@ -0,0 +1,16 @@
+package sqlc
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+}
